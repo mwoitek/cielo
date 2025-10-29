8b2e76aaf8b047e70d6e26731f742ac6a6cdf7b4
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-29 11:57:54 -0300

    style: re-format code

--- src/cielo.c
+++ src/cielo.c
@@ -1,6 +1,4 @@
-#include <math.h>
 #include "cielo.h"
+#include <math.h>
 
-float cube(float x) {
-    return powf(x, 3.0f);
-}
+float cube(float x) { return powf(x, 3.0F); }
--- src/cielo_lua.c
+++ src/cielo_lua.c
@@ -1,19 +1,18 @@
-#include <lua.h>
-#include <lauxlib.h>
 #include "cielo.h"
+#include <lauxlib.h>
+#include <lua.h>
 
 // Lua binding for cube()
-static int l_cube(lua_State *L) {
-    float x = (float)luaL_checknumber(L, 1);
-    lua_pushnumber(L, cube(x));
-    return 1;
+static int l_cube(lua_State *L)
+{
+	float x = (float)luaL_checknumber(L, 1);
+	lua_pushnumber(L, cube(x));
+	return 1;
 }
 
-int luaopen_cielo(lua_State *L) {
-    static const luaL_Reg funcs[] = {
-        {"cube", l_cube},
-        {NULL, NULL}
-    };
-    luaL_register(L, "cielo", funcs);
-    return 1;
+int luaopen_cielo(lua_State *L)
+{
+	static const luaL_Reg funcs[] = {{"cube", l_cube}, {NULL, NULL}};
+	luaL_register(L, "cielo", funcs);
+	return 1;
 }
