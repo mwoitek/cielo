5103a8f320a177d44d2290a45021dd0ca04bbcdf
Marcio Woitek <mwoitekjr@gmail.com>
2025-11-05 20:29:22 -0300

    feat: implement simple matrix operations

---
+++ src/matrix.h
@@ -0,0 +1,12 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#define MATRIX_DIM 3
+
+typedef double Matrix3[MATRIX_DIM][MATRIX_DIM];
+typedef double Vector3[MATRIX_DIM];
+
+void matrix_multiply(Matrix3 res, const Matrix3 A, const Matrix3 B);
+void matrix_multiply_vector(Vector3 res, const Matrix3 M, const Vector3 v);
+
+#endif // _MATRIX_H_
