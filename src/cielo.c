03a504606eb533049cad4cf393af7e076d743fc6
Marcio Woitek <mwoitekjr@gmail.com>
2025-11-06 03:39:39 -0300

    feat: implement RGB to XYZ conversion

--- src/cielo.c
+++ src/cielo.c
@@ -1,4 +1,5 @@
 #include "cielo.h"
+#include "matrix.h"
 #include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
@@ -7,6 +8,13 @@
 #include <string.h>
 
 const size_t RGB_HEX_LENGTH = 7;
+
+const Matrix3 RGB_TO_XYZ = {{0.4124564, 0.3575761, 0.1804375},
+			    {0.2126729, 0.7151522, 0.0721750},
+			    {0.0193339, 0.1191920, 0.9503041}};
+const Matrix3 XYZ_TO_RGB = {{3.2404542, -1.5371385, -0.4985314},
+			    {-0.9692660, 1.8760108, 0.0415560},
+			    {0.0556434, -0.2040259, 1.0572252}};
 
 // TODO: remove
 float cube(float x)
@@ -58,7 +66,40 @@ double rgb_gamma(double x)
 	return x > 0.0031308 ? 1.055 * pow(x, 1.0 / 2.4) - 0.055 : x * 12.92;
 }
 
-double rgb_gamma_inv(double x)
+double rgb_gamma_inverse(double x)
 {
 	return x > 0.04045 ? pow((x + 0.055) / 1.055, 2.4) : x / 12.92;
+}
+
+double rgb_clamp(double x)
+{
+	return fmax(0.0, fmin(x, 1.0));
+}
+
+Xyz rgb_to_xyz(const Rgb *rgb)
+{
+	const Vector3 linear_rgb = {rgb_gamma_inverse(rgb->r),
+				    rgb_gamma_inverse(rgb->g),
+				    rgb_gamma_inverse(rgb->b)};
+
+	Vector3 xyz_vec;
+	matrix_multiply_vector(xyz_vec, RGB_TO_XYZ, linear_rgb);
+
+	Xyz xyz = {.x = xyz_vec[0], .y = xyz_vec[1], .z = xyz_vec[2]};
+	return xyz;
+}
+
+Rgb xyz_to_rgb(const Xyz *xyz)
+{
+	const Vector3 xyz_vec = {xyz->x, xyz->y, xyz->z};
+
+	Vector3 rgb_vec;
+	matrix_multiply_vector(rgb_vec, XYZ_TO_RGB, xyz_vec);
+
+	for (size_t i = 0; i < MATRIX_DIM; i++) {
+		rgb_vec[i] = rgb_clamp(rgb_gamma(rgb_vec[i]));
+	}
+
+	Rgb rgb = {.r = rgb_vec[0], .g = rgb_vec[1], .b = rgb_vec[2]};
+	return rgb;
 }
--- src/cielo.h
+++ src/cielo.h
@@ -12,10 +12,20 @@ typedef struct {
 	double b;
 } Rgb;
 
+typedef struct {
+	double x;
+	double y;
+	double z;
+} Xyz;
+
 bool rgb_validate_hex(const char *hex);
 Rgb rgb_from_hex(const char *hex, bool *ok);
 
+double rgb_gamma_inverse(double x);
 double rgb_gamma(double x);
-double rgb_gamma_inv(double x);
+double rgb_clamp(double x);
+
+Xyz rgb_to_xyz(const Rgb *rgb);
+Rgb xyz_to_rgb(const Xyz *xyz);
 
 #endif // _CIELO_H_
