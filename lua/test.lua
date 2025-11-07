fafbd529c5a77095df67a4f17ad85b4db23f50fd
Marcio Woitek <mwoitekjr@gmail.com>
2025-11-07 10:04:24 -0300

    test: add tests for Lua module

--- lua/test.lua
+++ lua/test.lua
@@ -1,2 +1,14 @@
 local cielo = require("cielo")
-print(cielo.cube(3.5)) -- 42.875
+
+local violet_hex = "#EE82EE"
+local violet_rgb = cielo.rgb_from_hex(violet_hex)
+assert(type(violet_rgb) == "table")
+
+print(string.format("Violet: Red   = %.5f", violet_rgb.r))
+print(string.format("Violet: Green = %.5f", violet_rgb.g))
+print(string.format("Violet: Blue  = %.5f", violet_rgb.b))
+
+local invalid_hex = "61de2a"
+local ok, msg = pcall(cielo.rgb_from_hex, invalid_hex)
+assert(not ok)
+print(msg)
